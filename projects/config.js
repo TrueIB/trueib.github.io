2b35ba6d752552b22b109f823f95b8013b001304
TrueIB <185382252970@163.com>
2025-10-05 16:11:40 +0800

    使用 iframe 优化外部网站的显示

--- projects/config.js
+++ projects/config.js
@@ -1,10 +1,32 @@
+function replace(url, href, file) {
+  const urlNum = {
+    'q61': 9
+  }
+  let whref = '';
+  for (let i = 0; i < window.location.href.length - urlNum[file]; i++) {
+    whref += window.location.href[i];
+  }
+  const urlObject = new URL(whref + '/' + url);
+  urlObject.searchParams.set('src', href); const link = document.createElement('a');
+  link.href = urlObject.toString();
+  link.target = '_blank';
+  document.body.appendChild(link);
+  link.click();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const subject = localStorage.getItem("trueib-subject");
   const body = document.querySelector("body");
   document.documentElement.setAttribute("data-subject", subject ? subject : "light");
-  body.innerHTML += `<footer class="footer">Copyright &copy;${new Date().getFullYear()} TrueIB</footer>`;const codeContents = document.querySelectorAll("pre");
+  body.innerHTML += `<footer class="footer">Copyright &copy;${new Date().getFullYear()} TrueIB</footer>`; const codeContents = document.querySelectorAll("pre");
   let isDragging = false;
   let startX, scrollLeft;
+  const aArray = document.querySelectorAll('.a');
+  for (let a of aArray) {
+    a.addEventListener('click', () => {
+      replace(`iframe.html`, a.innerHTML, a.id);
+    });
+  }
   for (let codeContent of codeContents) {
     codeContent.addEventListener('touchstart', (e) => {
       isDragging = true;
