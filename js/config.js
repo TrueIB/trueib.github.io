9a4f10a6576c67da5186920e37a11902f23d9b3d
TrueIB <185382252970@163.com>
2025-12-13 22:01:52 +0800

    优化多处细节，新增功能

--- js/config.js
+++ js/config.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const subject = localStorage.getItem("trueib-subject");
   const body = document.querySelector("body");
+  const image = document.querySelector('#image');
+  if (image) {
+    image.src = `home-${subject}.png`;
+  }
   document.documentElement.setAttribute("data-subject", subject ? subject : "light");
   body.innerHTML += `
   <footer class="footer">Copyright &copy;
@@ -12,5 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener("message", function (event) {
   if (event.data && event.data.type === "THEME_CHANGE") {
     this.document.documentElement.setAttribute("data-subject", event.data.subject);
+    const image = this.document.querySelector('#image');
+    if (image) {
+      image.src = `home-${event.data.subject}.png`;
+    }
   }
 });
